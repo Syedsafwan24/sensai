db4a949a43dd68bb3966d6b3105628a4440a89b4
Syedsafwan24 <136835578+Syedsafwan24@users.noreply.github.com>
2025-02-28 16:15:14 +0530

    completed hero section

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import HeroSection from '@/components/hero';
+
+function page() {
+	return (
+		<div>
+			<div className='grid-background'></div>
+			<HeroSection />
+		</div>
+	);
+}
+
+export default page;
