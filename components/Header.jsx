4f4b6870325402b84f05d583d2c9ae35ddf20a47
Syedsafwan24 <136835578+Syedsafwan24@users.noreply.github.com>
2025-03-01 17:47:54 +0530

    added first user in db

--- components/Header.jsx
+++ components/Header.jsx
@@ -16,8 +16,10 @@ import {
 	DropdownMenuItem,
 	DropdownMenuTrigger,
 } from './ui/dropdown-menu';
+import { checkUser } from '@/lib/checkUser';
 
-function Header() {
+async function Header() {
+	await checkUser();
 	return (
 		<header
 			className='fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60 overflow-hidden'
